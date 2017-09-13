3d395da807431556db17c001dd05f0b40ddc0d0b
Fernando Ramirez <Fernando.Ramirez@ltu.se>
2017-09-13 19:06:56 +0200

    Add SimpleServiceDiscovery
    From: https://bitbucket.org/fedeselmer/simpleservicediscovery
    
    This has to be changed for using sources and building them instead of
    grabbing binaries. Also, use environment variables for the ports.

--- core/ntpd/scripts/entrypoint.sh
+++ core/ntpd/scripts/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+rm -f /tsig/tsig
+
 # Configure the server  
 SERVER_HOSTNAME=`hostname -f`
 SERVER_DOMAIN=`hostname -d`
@@ -12,6 +14,7 @@ chmod 755 /etc/rndc.key
 echo "key.$SERVER_DOMAIN." > /out/tsig
 grep secret /etc/named.conf | cut -d'"' -f2 >> /out/tsig
 chmod 777 /out/tsig
+cp /out/tsig /tsig/tsig
 
 # If no argument received, run server
 # Otherwise, run the arguments received
---
+++ core/simpleservicediscovery/scripts/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+sleep 2
+while [ ! -f /tsig/tsig ]
+do
+  sleep 2
+done
+
+echo "# ARROWHEAD MODULE PROPERTIES
+# Core Services Discovery
+core.server=ntpd
+core.domain=docker.ahf
+core.hostname=localhost
+core.tsig=/tsig/tsig
+
+# Truststore/keystore
+truststore.file=./SimpleServiceRegistry.jks
+truststore.password=abc1234
+keystore.file=./SimpleServiceRegistry.jks
+keystore.password=abc1234
+
+# Authorisation
+# Backup URL if not found in SR
+authorisation.url=https://glassfish:8181/authorisation-control
+
+# Orchestration
+# Backup URL if not found in SR
+orchestration.url=https://glassfish:8181/orchestration-store
+# Orchestration Store poll interval
+orchestration.monitor.interval=100
+
+
+# Define supported consumption service types
+service.consume.support=\"\"
+service.consume.polling.interval=10
+simpleservicediscovery.coap.port=5683
+simpleservicediscovery.rest.port=8045
+simpleservicediscovery.ip=0.0.0.0
+" > SimpleServiceRegistry.properties
+
+java -jar SimpleServiceDiscovery.jar
+
+exec "$@"
