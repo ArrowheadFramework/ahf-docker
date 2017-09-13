b8115a0dbb65ac24c49afc8234c86d2ce35c7f03
Fernando Ramirez <Fernando.Ramirez@ltu.se>
2017-09-13 12:42:53 +0200

    Print tsig file for easy access
    The file will be available from ./out/tsig after starting the ntpd
    container.

--- core/ntpd/scripts/entrypoint.sh
+++ core/ntpd/scripts/entrypoint.sh
@@ -3,11 +3,15 @@ set -e
 
 # Configure the server  
 SERVER_HOSTNAME=`hostname -f`
+SERVER_DOMAIN=`hostname -d`
 service named stop
 rndc-confgen -a -r /dev/urandom
 chown root /etc/rndc.key
 chmod 755 /etc/rndc.key
 /ahf/dns-configure.sh
+echo "key.$SERVER_DOMAIN." > /out/tsig
+grep secret /etc/named.conf | cut -d'"' -f2 >> /out/tsig
+chmod 777 /out/tsig
 
 # If no argument received, run server
 # Otherwise, run the arguments received
